fb6076bba7a8084bdc924cfb5f76715f4a4759ca
Pinkablee <101295543+Pinkablee@users.noreply.github.com>
2023-10-19 20:59:31 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,8 @@ game 'gta5'
 lua54 'yes'
 
 author 'Pinkable'
-version '1.0.3'
-description 'Vehicle Spawner (p-spawner)'
+version '1.0.0'
+description '(p-spawner) A modern, well-rounded FiveM vehicle spawner system.'
 
 shared_scripts {
     '@ox_lib/init.lua',
