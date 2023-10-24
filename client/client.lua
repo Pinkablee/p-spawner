5842530b9159d2456cf31e5bf07b5bacfa3d3379
Pinkablee <101295543+Pinkablee@users.noreply.github.com>
2023-10-24 13:39:56 -0700

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,11 +1,9 @@
 local notified = false
 
-local function OpenContext(coords, heading)	
+local function OpenContext(coords, heading)
     local options = {}
 
-    for i=1, #cfg?.vehicles do
-        local vehicle = cfg?.vehicles[i]
-
+    for i, vehicle in pairs(cfg?.vehicles) do
         table.insert(options, {
             title = vehicle.label,
             description = 'Spawn this vehicle!',
@@ -20,9 +18,9 @@ local function OpenContext(coords, heading)
     end
 
     lib.registerContext({
-        id = 'vehicle_menu', 
-        menu = 'vehicle_spawner', 
-        title = 'Vehicle Spawner', 
+        id = 'vehicle_menu',
+        menu = 'vehicle_spawner',
+        title = 'Vehicle Spawner',
         options = options
     })
     lib.showContext('vehicle_menu')
@@ -30,49 +28,46 @@ end
 
 CreateThread(function()
     while true do
-		local sleep = 500
-
+        local sleep = 500
         local coords = GetEntityCoords(cache.ped)
 
-        for i=1, #cfg?.locations do
-            local location = cfg?.locations[i]
-
+        for i, location in pairs(cfg?.locations) do
             local dist = #(coords - location.marker)
 
             if dist < 5.0 then
-				sleep = 0
-
+                sleep = 0
                 DrawMarker(2, location.marker.x, location.marker.y, location.marker.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 255, 150, false, false, 2, true, nil, nil, false)
-				
+
                 if dist < 1.5 then
-					if not notified then
-						notified = true
-						lib.showTextUI('[E] - Spawn a Vehicle')
-					end
+                    if not notified then
+                        notified = true
+                        lib.showTextUI('[E] - Spawn a Vehicle')
+                    end
 
                     local pressed = IsControlJustPressed(0, 38)
-					if pressed then
-						OpenContext(location.coords, location.heading)
-					end
-                elseif notified then
-					notified = false
-					lib.hideTextUI()
-                end
-			end
-		end
 
-		Wait(sleep)
-	end
+                    if pressed then
+                        OpenContext(location.coords, location.heading)
+                    end
+                elseif notified then
+                    notified = false
+                    lib.hideTextUI()
+                end
+            end
+        end
+
+        Wait(sleep)
+    end
 end)
 
 RegisterNetEvent('spawnVehicle', function(data)
-    if data.model then return end
+    if not data.model then return end
 
     local hash = GetHashKey(data.model)
     RequestModel(hash)
 
     while not HasModelLoaded(hash) do
-    	RequestModel(hash) 
+        RequestModel(hash)
         Wait(100)
     end
 
