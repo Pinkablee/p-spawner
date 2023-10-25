dd3a673fa82c8a5858fdb49ddb7d462ef053bc95
Pinkablee <101295543+Pinkablee@users.noreply.github.com>
2023-10-24 18:53:18 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,12 @@
-cfg = {}
+cfg = {
+    --Locations configuration
+    locations = {
+        {marker = vec3(-20.085262, -1823.228027, 26.795788), coords = vec3(-23.804525, -1826.857178, 25.701361), heading = 56.832691},
+    },
 
-cfg.locations = {
-    {marker = vec3(-20.085262, -1823.228027, 26.795788), coords = vec3(-23.804525, -1826.857178, 25.701361), heading = 56.832691},
-}
-
-cfg.vehicles = {
-    {label = "Elegy", model = "elegy"},
-    {label = "Sultan RS", model = "sultanrs"}
+    --Vehicles configuration
+    vehicles = {
+        {label = "Elegy", model = "elegy"},
+        {label = "Sultan RS", model = "sultanrs"}
+    },
 }
