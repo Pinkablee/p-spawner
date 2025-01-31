6cb58cdbd28b0941921c86da32129254edd3cbf1
Pinkable <101295543+Pinkablee@users.noreply.github.com>
2025-01-31 01:44:25 -0800

    Update version.lua

--- server/version.lua
+++ server/version.lua
@@ -1,12 +1,15 @@
 CreateThread(function()
     local url = "https://raw.githubusercontent.com/pinkablee/versions/main/spawner.txt"
-    local version = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    local currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
 
-    PerformHttpRequest(url, function(errorCode, resultData, resultHeaders)
-        if resultData and errorCode == 200 then
-            resultData = resultData:match("%d%.%d+%.%d+")
-            if version < resultData then
-                print(("^3[ALERT]^0: A new version is available. (Current Version: ^1%s^0, Latest Version: ^2%s^0)"):format(version, resultData))
+    PerformHttpRequest(url, function(statusCode, responseBody)
+        if statusCode == 200 and responseBody then
+            local latestVersion = responseBody:match("%d+%.%d+%.%d+")
+            if latestVersion and currentVersion < latestVersion then
+                print((
+                    "^3[ALERT]^0: A new version is available. " ..
+                    "(Current Version: ^1%s^0, Latest Version: ^2%s^0)"
+                ):format(currentVersion, latestVersion))
             end
         end
     end, "GET")
