abd454560a0f121f3945fbd629e80c47451ed7c8
Pinkable <101295543+Pinkablee@users.noreply.github.com>
2025-01-31 01:31:59 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,16 @@
-cfg = {
-    --Locations configuration
-    locations = {
-        {marker = vec3(-20.085262, -1823.228027, 26.795788), coords = vec3(-23.804525, -1826.857178, 25.701361), heading = 56.832691},
-    },
+Config = {}
 
-    --Vehicles configuration
-    vehicles = {
-        {label = "Elegy", model = "elegy"},
-        {label = "Sultan RS", model = "sultanrs"}
-    },
+--Locations configuration
+Config.Locations = {
+    {
+        Marker = vec3(-20.085262, -1823.228027, 26.795788),
+        SpawnCoords = vec3(-23.804525, -1826.857178, 25.701361),
+        Heading = 56.832691
+    },    
+}
+
+--Vehicles configuration
+Config.Vehicles = {
+    { Label = "Elegy", Model = "elegy" },
+    { Label = "Sultan RS", Model = "sultanrs" }
 }
